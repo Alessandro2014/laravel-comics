d3981e44437e799265bc5d7cd3a726c9c3a9d909
Alessandro2014 <alessandromerenda22@gmail.com>
2021-10-01 16:20:33 +0200

    prova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Home</title>
 </head>
+
 <body>
-    <h1>hello word</h1>
+    <header class="container">
+        <div class="row">
+            <figure>
+                <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+            </figure>
+            <!-- STAMPA LINK DI NAVIGAZIONE -->
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Collectibles</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">Fans</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+            </ul>
+        </div>
+    </header>
 </body>
+
 </html>
